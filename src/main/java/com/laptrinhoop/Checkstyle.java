f6c4b0d0fe9b9e0cc8be3c8d84556019c4ccf83e
tuanvuu <55964246+tuanvuu@users.noreply.github.com>
2024-09-21 19:44:02 +0700

    Create Checkstyle.java

---
+++ src/main/java/com/laptrinhoop/Checkstyle.java
@@ -0,0 +1,22 @@
+package com.laptrinhoop.checkstyle;
+
+public class Checkstyle {
+    public void check() {
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+            System.out.println(c);
+    }
+
+    public void check2() {
+        int a = 1;
+        int b = 2;
+    }
+
+    public String check3() {
+        String a = "a";
+        String b = "b";
+        System.out.println(a);
+        return a;
+    }
+}
